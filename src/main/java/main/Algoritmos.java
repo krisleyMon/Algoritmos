49bca76967d08eaf98f6da0fb10255d168203e88
krisleyMon <krisley.montoya@ucr.ac.cr>
2026-09-11 15:21:10 -0600

    Primer commit

---
+++ src/main/java/main/Algoritmos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package main;
+
+/**
+ *
+ * @author krisl
+ */
+public class Algoritmos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
