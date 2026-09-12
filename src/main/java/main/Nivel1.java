d44c12a6b9e3a4f173615bf3b4227d2b6ba29382
krisleyMon <krisley.montoya@ucr.ac.cr>
2026-09-11 18:51:11 -0600

    Nivel 1 algoritmos

---
+++ src/main/java/main/Nivel1.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main;
+
+/**
+ *
+ * @author krisl
+ */
+public class Nivel1 {
+   
+    public void modtrarLongitudYUltimoNumero(){
+        double[] numero = {2.5,5.5,7.7,4.9};
+        
+      System.out.print("La longitud del arreglo es"+numero.length);
+      System.out.print("La longitud del arreglo es"+numero[numero.length-1]);  
+    }
+    
+    
+    
+    
+}
--- src/main/java/main/Nivel2.java
+++ src/main/java/main/Nivel2.java
@@ -1,15 +1,13 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 package main;
 
 /**
  *
  * @author krisl
  */
-public class Algoritmos {
-public static void main(String[] args) {
-   Menu ñ = new Menu();
- }
+public class Nivel2 {
+    
 }
